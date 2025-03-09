4dfedeaa2472eb06247108448779d11fc0148fc3
Shining Qiao <2988779Q@student.gla.ac.uk>
2025-03-09 22:37:38 +0800

    qsn

--- app_user/views.py
+++ app_user/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import render
 from flask import redirect
 from app_user.models import User
 from visualization.settings import TEMPLATE_PATHS
+import json
+from django.http import JsonResponse
+
 
 
 # view for user to login
@@ -58,3 +61,42 @@ def user_center(request):
     message = "user center"
     User.user_center(request.session["username"])
     return render(request, to_page, {"message": message})
+
+def user_profile(request):
+    """获取用户信息"""
+    user = request.use
+    user_info = {
+        "first_name": user.first_name,
+        "last_name": user.last_name,
+        "email": user.email,
+        "phone": user.phone,
+        "gender": user.gender,
+        "birth": user.birth.strftime("%Y-%m-%d") if user.birth else "",
+        "location": user.location,
+        "introduction": user.introduction,
+    }
+    return JsonResponse(user_info)
+
+
+def update_profile(request):
+    if request.method == "POST":
+        try:
+            # user = request.user  # 获取当前用户
+            user_id= request.session.get("id")
+            user = User.objects.get(username=request.session.get("username")) 
+            print(user_id)    
+            # 更新用户信息
+            user.first_name = request.POST.get("first_name", user.first_name)
+            # user.last_name = data.get("last_name", user.last_name)
+            # user.phone = data.get("phone", user.phone)
+            # user.gender = data.get("gender", user.gender)
+            # user.birth = data.get("birth", user.birth)
+            # user.location = data.get("location", user.location)
+            # user.introduction = data.get("introduction", user.introduction)
+
+            user.save()  # 保存更改
+            return JsonResponse({"message": "Profile updated successfully!"})
+        except Exception as e:
+            return JsonResponse({"error": str(e)}, status=400)
+
+    return JsonResponse({"error": "Invalid request"}, status=400)
